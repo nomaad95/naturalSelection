86bfbde387fd158288deda267a6145c7ebec2666
nomaad95 <camille.bamboute@hotmail.fr>
2023-10-24 16:51:45 +0200

    normalization of inputs

--- main.py
+++ main.py
@@ -113,16 +113,17 @@ def main(genomes, config):
     for _, genome in genomes:
         network = neat.nn.FeedForwardNetwork.create(genome,config)
         networks.append(network)
-        mouses.append(Mouse(200,700))
+        mouses.append(Mouse(230,700))
         genome.fitness = 0
         ges.append(genome)
     #obstacle2 = Obstacle(400,200)
     obstacles = []
     # for i in range(10):
     #     obstacles.append(Obstacle(random.randint(0,600),random.randint(0,600)))
+    obstacles.append(Obstacle(400,250))
+    obstacles.append(Obstacle(250,450))
     obstacles.append(Obstacle(150,600))
-    obstacles.append(Obstacle(300,300))
-    # obstacles.append(Obstacle(500,100))
+    obstacles.append(Obstacle(200,300))
 
 
     collision = False
@@ -138,78 +139,80 @@ def main(genomes, config):
             pass
         else:
             run = False
-            print("quit")
-            break
         # if not collide:
         #     mouse.turn(0.1)
         #     mouse.move(0.5)
         for x,mouse in enumerate(mouses):
+            if mouse.rotation > 170 and mouse.rotation < 185:
+                mouses.remove(mouse)
             if mouse.y > 10:
-                remaining_obstacles = []
-                for obstacle in obstacles:
-                    # adds obstacles still above the mouse
-                    if obstacle.y < mouse.y:
-                        remaining_obstacles.append(obstacle)
-                print(len(remaining_obstacles))
-                obstacle = obstacles[0]
-                smallest_distance = abs(mouse.y - obstacle.y)
-                # Define the smallest distance by the distance between the mouse
-                # And the first obstacle
-                # As the mouse is moving fast, sometimes a passed obstacle can still
-                # be the current obstacle selected.
-                # Hence, abs() is used to prevent from any negative values
-                for obstacle_loop in remaining_obstacles:
-                    if mouse.y - obstacle_loop.y < smallest_distance and (mouse.y > obstacle_loop.y) and (mouse.y - obstacle_loop.y >= 0):
-                        print("changement obstacle")
-                        obstacle = obstacle_loop
-                        smallest_distance = mouse.y - obstacle.y
-                mouse.move(1.7)
+                mouse.move(2)
                 ges[x].fitness += 0.1
-                #output = networks[x].activate(mouse.x,abs(mouse.x - obstacles[0].x), mouse.y, abs(mouse.y - obstacles[0].y))
-                # activation function for obstacle detection
+                remaining_obstacles = []
+                for obs in obstacles:
+                    # adds obstacles still above the mouse
+                    if obs.y < mouse.y:
+                        remaining_obstacles.append(obs)
+                if len(remaining_obstacles) < len(obstacles):
+                    # Tells the NN the aim is to go up
+                    ges[x].fitness += 1
+
                 if len(remaining_obstacles) > 0:
-                    output = networks[x].activate((mouse.x, abs(mouse.x - obstacle.x), mouse.y))
-                    output2 = networks[x].activate((mouse.y,mouse.y - obstacle.y, mouse.y))
+                    remaining_obstacles.sort(key=lambda x:x.y, reverse=True)
+                    # Takes the closest obstacle from the mouse
+                    # Depending on y coordinates
+                    obstacle = remaining_obstacles[0]
+                    next_obstacle = obstacle
+                    if len(remaining_obstacles) > 1:
+                        next_obstacle = remaining_obstacles[1]
+                    # activation function for obstacle detection
+
+                    output = networks[x].activate((abs(mouse.y - obstacle.y)/800, abs(mouse.x - obstacle.x)/600, abs(obstacle.x - next_obstacle.x)/600))
                     sign = 1
                     angle = 0
-                    if output[0] > 0.5:
+                    if output[0] > 0.99:
+                        print(output)
                         if mouse.y > obstacle.y:
                             if(mouse.x - obstacle.x) < 0:
                                 sign = 1
                             else:
                                 sign = -1
-                            angle = sign * (1 -(1/abs(mouse.x - obstacle.x + 0.01)))
-                    elif output2[0] > 0.5:
+                            angle = sign * (1 -(1/abs(mouse.x - obstacle.x)))
+                    else:
                         if mouse.rotation > 0 and mouse.rotation < 180:
                             sign = -1
                         else:
                             sign = 1
                         angle = sign * 1
+                    if mouse.rotation > 90 and mouse.rotation < 270:
+                        ges[x].fitness -= 10
                     mouse.turn(angle)
                 else:
                     # depending on th rotation, the mouse will turn left or right
                     # The angle is defined on [0,1]
                     # If there is no more obstacles, the angle will be multiplied by itself
                     # Decreasing the angle to 0, the mouse will go up
-                    if mouse.rotation > 0 and mouse.rotation < 180:
-                        sign = -1
+                    angle = 0
+                    if mouse.rotation > 0 and mouse.rotation < 270:
+                        angle = -1
                     else:
-                        sign = 1
-                    mouse.turn(sign*angle)
-                    print("no obstacles")
+                        angle = 1
+                    mouse.turn(angle* (1 - (mouse.rotation/(mouse.rotation + 360))))
             else:
                 selecteds += 1
+                ges[x].fitness += 5
 
         draw_window(win, mouses, obstacles)
         for obstacle in obstacles:
             for x,mouse in enumerate(mouses):
-                if obstacle.collide(mouse) or mouse.x > 590 or mouse.y > 700:
+                if obstacle.collide(mouse) or mouse.x > 590 or mouse.y > 700 or mouse.x < 0:
                     # decreases the fitness score of the neural network
                     # who collided the obstacle
                     ges[x].fitness -= 1
                     mouses.pop(x)
                     networks.pop(x)
-                elif mouse.get_rect().top == 0:
+                elif mouse.y < 20:
+                    mouses.pop(x)
                     ges[x].fitness += 5
 def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
